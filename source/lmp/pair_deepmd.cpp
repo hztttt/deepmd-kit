9f8f945d2dfc63bfb3a89ac5de61fa0d55dcf739
Duo <50307526+iProzd@users.noreply.github.com>
2024-09-28 19:45:30 +0800

    fix tensorflow bug

--- source/lmp/pair_deepmd.cpp
+++ source/lmp/pair_deepmd.cpp
@@ -579,16 +579,11 @@ void PairDeepMD::compute(int eflag, int vflag) {
           }
         } else {
           try {
-            // vector<double> deatom(nall * 1, 0);
-	          // vector<double> dvatom(nall * 9, 0);
-            // deep_pot.compute(dener, dforce, dforce_mag, dvirial, deatom, dvatom,
-            //                  dcoord, dspin, dtype, dbox, nghost, lmp_list, ago,
-            //                  fparam, daparam);
-            const vector<double> & dcoord_const = dcoord;
-            const vector<double> & dspin_const = dspin;
-            deep_pot.compute(dener, dforce, dforce_mag, dvirial, dcoord_const, dspin_const,
-                             dtype, dbox, nghost, lmp_list, ago, fparam,
-                             daparam);
+            const vector<double> &dcoord_const = dcoord;
+            const vector<double> &dspin_const = dspin;
+            deep_pot.compute(dener, dforce, dforce_mag, dvirial, dcoord_const,
+                             dspin_const, dtype, dbox, nghost, lmp_list, ago,
+                             fparam, daparam);
           } catch (deepmd_compat::deepmd_exception &e) {
             error->one(FLERR, e.what());
           }
@@ -608,6 +603,7 @@ void PairDeepMD::compute(int eflag, int vflag) {
           }
         } else {
           try {
+            std::cout << "calculate atomic energy" << std::endl;
             deep_pot.compute(dener, dforce, dforce_mag, dvirial, deatom, dvatom,
                              dcoord, dspin, dtype, dbox, nghost, lmp_list, ago,
                              fparam, daparam);
@@ -682,7 +678,7 @@ void PairDeepMD::compute(int eflag, int vflag) {
         if (!(eflag_atom || cvflag_atom)) {
           try {
             deep_pot_model_devi.compute(all_energy, all_force, all_force_mag,
-                                        all_virial, dcoord, dspin, dtype, dbox, 
+                                        all_virial, dcoord, dspin, dtype, dbox,
                                         nghost, lmp_list, ago, fparam, daparam);
           } catch (deepmd_compat::deepmd_exception &e) {
             error->one(FLERR, e.what());
