b31a396e2f4dcd992952c4ea3f1cef1a93c93e13
hztttt <940755193@qq.com>
2024-10-23 23:32:30 +0800

    add spin nlist for tf

--- source/api_cc/include/DeepPotTF.h
+++ source/api_cc/include/DeepPotTF.h
@@ -396,6 +396,14 @@ class DeepPotTF : public DeepPotBase {
               const int numb_types_spin,
               const std::vector<VALUETYPE>& virtual_len,
               const std::vector<VALUETYPE>& spin_norm);
+
+  template <typename VALUETYPE>
+  void extend_nlist(std::vector<VALUETYPE>& extend_dcoord,
+		    std::vector<int>& extend_atype,
+		    const std::vector<VALUETYPE>& dcoord_,
+                    const std::vector<VALUETYPE>& dspin_,
+                    const std::vector<int>& datype_);
+
   void cum_sum(std::map<int, int>&, std::map<int, int>&);
 
  private:
